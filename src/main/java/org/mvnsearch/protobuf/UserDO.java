bd0848c9d8f1f06a312ee2eb23cf3addf6af2b41
linux_china <libing.chen@gmail.com>
2019-01-19 10:57:34 -0800

    User javabean

---
+++ src/main/java/org/mvnsearch/protobuf/UserDO.java
@@ -0,0 +1,55 @@
+package org.mvnsearch.protobuf;
+
+/**
+ * User javabean
+ *
+ * @author linux_china
+ */
+public class UserDO {
+    private Integer id;
+    private String name;
+    private String email;
+    private String phone;
+
+    public UserDO() {
+    }
+
+    public UserDO(Integer id, String name, String email, String phone) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
