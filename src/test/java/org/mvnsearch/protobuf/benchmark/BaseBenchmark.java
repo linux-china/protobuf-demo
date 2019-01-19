c6694cc819c8b36f4b3b1e51c2ff3456861abce3
linux_china <libing.chen@gmail.com>
2019-01-19 11:05:21 -0800

    base benchmark

---
+++ src/test/java/org/mvnsearch/protobuf/benchmark/BaseBenchmark.java
@@ -0,0 +1,26 @@
+package org.mvnsearch.protobuf.benchmark;
+
+import org.mvnsearch.protobuf.UserDO;
+import org.mvnsearch.protobuf.UserKDO;
+import org.mvnsearch.user.UserPB;
+import org.openjdk.jmh.annotations.*;
+
+import java.util.concurrent.TimeUnit;
+
+@Fork(1)
+@Warmup(iterations = 1)
+@Measurement(iterations = 2)
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(TimeUnit.SECONDS)
+@State(Scope.Benchmark)
+public class BaseBenchmark {
+    protected UserPB userPB = UserPB.newBuilder()
+            .setId(1)
+            .setName("Jackie")
+            .setEmail("demo@demo.com")
+            .setPhone("186").build();
+
+    protected UserDO userDO = new UserDO(1, "Jackie", "demo@demo.com", "186");
+    protected UserKDO userKDO = new UserKDO(1, "Jackie", "demo@demo.com", "186");
+
+}
