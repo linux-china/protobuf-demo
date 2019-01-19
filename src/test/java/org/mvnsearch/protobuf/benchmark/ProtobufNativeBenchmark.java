24c140200730d02ec3de540571a4634cb3e15674
linux_china <libing.chen@gmail.com>
2019-01-19 11:03:46 -0800

    protobuf native benchmark

---
+++ src/test/java/org/mvnsearch/protobuf/benchmark/ProtobufNativeBenchmark.java
@@ -0,0 +1,19 @@
+package org.mvnsearch.protobuf.benchmark;
+
+import jmh.mbr.junit5.Microbenchmark;
+import org.mvnsearch.user.UserPB;
+import org.openjdk.jmh.annotations.Benchmark;
+
+/**
+ * protobuf native benchmark
+ *
+ * @author linux_china
+ */
+@Microbenchmark
+public class ProtobufNativeBenchmark extends BaseBenchmark {
+
+    @Benchmark
+    public void benchmark() throws Exception {
+        UserPB.parseFrom(userPB.toByteArray());
+    }
+}
