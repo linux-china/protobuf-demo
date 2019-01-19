a1cbf929692233277cb2f7eb73ee5f92a372cd95
linux_china <libing.chen@gmail.com>
2019-01-19 12:32:06 -0800

    polishing

--- src/test/java/org/mvnsearch/protobuf/benchmark/ProtobufNativeBenchmark.java
+++ src/test/java/org/mvnsearch/protobuf/benchmark/ProtobufNativeBenchmark.java
@@ -1,6 +1,7 @@
 package org.mvnsearch.protobuf.benchmark;
 
 import jmh.mbr.junit5.Microbenchmark;
+import org.mvnsearch.protobuf.BaseBenchmark;
 import org.mvnsearch.user.UserPB;
 import org.openjdk.jmh.annotations.Benchmark;
 
