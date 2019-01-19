c754e53003925a58cc19e2ca1c660df9b132c44d
linux_china <libing.chen@gmail.com>
2019-01-19 11:06:07 -0800

    protostuff test

---
+++ src/test/java/org/mvnsearch/protobuf/ProtostuffTest.java
@@ -0,0 +1,31 @@
+package org.mvnsearch.protobuf;
+
+
+import io.protostuff.LinkedBuffer;
+import io.protostuff.ProtostuffIOUtil;
+import io.protostuff.Schema;
+import io.protostuff.runtime.RuntimeSchema;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * protostuff test
+ *
+ * @author linux_china
+ */
+public class ProtostuffTest {
+    private static Schema<UserDO> userDOSchema = RuntimeSchema.getSchema(UserDO.class);
+
+    @Test
+    public void testEncodingDecoding() {
+        UserDO userDO = new UserDO(1, "Jackie", "demo@demo.com", "186");
+        final byte[] protostuff;
+        // Re-use (manage) this buffer to avoid allocating on every serialization   buffer.clear()  performance still good
+        LinkedBuffer buffer = LinkedBuffer.allocate(512);
+        protostuff = ProtostuffIOUtil.toByteArray(userDO, userDOSchema, buffer);
+        UserDO userDO2 = userDOSchema.newMessage();
+        ProtostuffIOUtil.mergeFrom(protostuff, userDO2, userDOSchema);
+        System.out.println(userDO2.getName());
+        Assertions.assertEquals(userDO.getName(), userDO2.getName());
+    }
+}
