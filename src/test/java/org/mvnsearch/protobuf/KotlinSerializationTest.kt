d76409dbe480d7d1254371e6e09b3b5148ad19a6
linux_china <libing.chen@gmail.com>
2019-01-19 11:05:35 -0800

    Kotlin Serialization test

---
+++ src/test/java/org/mvnsearch/protobuf/KotlinSerializationTest.kt
@@ -0,0 +1,28 @@
+package org.mvnsearch.protobuf
+
+import kotlinx.serialization.*
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.protobuf.ProtoBuf
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+/**
+ * Kotlin Serialization test
+ *
+ * @author linux_china
+ */
+@ImplicitReflectionSerializer
+class KotlinSerializationTest {
+
+    @Test
+    fun testSerialization() {
+        val user = UserKDO(1, "Jackie", "xxx@demo.com", "186")
+        val pbData = ProtoBuf.dump(user)
+        val pbUser = ProtoBuf.load<UserKDO>(pbData);
+        println(pbUser.name)
+        val jsonText =  Json.stringify(user)
+        val jsonUser = Json.parse<UserKDO>(jsonText)
+        println(jsonUser.name)
+        Assertions.assertEquals(pbUser.name, jsonUser.name)
+    }
+}
