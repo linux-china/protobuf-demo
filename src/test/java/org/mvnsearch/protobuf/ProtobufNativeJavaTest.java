2801d4e43a6938cf618bed62c1966a9711d2fd7c
linux_china <libing.chen@gmail.com>
2019-06-14 16:25:29 -0700

    add test Any

--- src/test/java/org/mvnsearch/protobuf/ProtobufNativeJavaTest.java
+++ src/test/java/org/mvnsearch/protobuf/ProtobufNativeJavaTest.java
@@ -1,5 +1,6 @@
 package org.mvnsearch.protobuf;
 
+import com.google.protobuf.Any;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mvnsearch.user.UserPB;
@@ -22,5 +23,18 @@ public class ProtobufNativeJavaTest {
         UserPB pbUser = UserPB.parseFrom(pbBytes);
         System.out.println(pbUser.getName());
         Assertions.assertEquals(pbUser.getName(), user.getName());
+    }
+
+    @Test
+    public void testAnyType() throws Exception {
+        UserPB user = UserPB.newBuilder()
+                .setId(1)
+                .setName("Jackie")
+                .setEmail("demo@demo.com")
+                .setPhone("186").build();
+        Any any1 = Any.pack(user);
+        Any any = Any.parseFrom(any1.toByteArray());
+        UserPB user2 = any.unpack(UserPB.class);
+        System.out.println(user2.getName());
     }
 }
