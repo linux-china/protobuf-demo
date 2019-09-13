00b0c84e2bf7581d334cfc626ee45e5baa5e0940
linux_china <libing.chen@gmail.com>
2019-09-13 09:06:17 -0700

    moved

--- src/test/kotlin/org/mvnsearch/protobuf/benchmark/KotlinSerializationTest.kt
+++ src/test/kotlin/org/mvnsearch/protobuf/benchmark/KotlinSerializationTest.kt
@@ -1,10 +1,11 @@
-package org.mvnsearch.protobuf
+package org.mvnsearch.protobuf.benchmark
 
 import kotlinx.serialization.*
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.protobuf.ProtoBuf
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.mvnsearch.protobuf.UserKDO
 
 /**
  * Kotlin Serialization test
