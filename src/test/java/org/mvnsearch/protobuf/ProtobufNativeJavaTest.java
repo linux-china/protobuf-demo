709fb438832f30c8d634afb8a9f0d49d5de0f744
linux_china <libing.chen@gmail.com>
2021-08-25 12:14:48 -0700

    Add testMetadata

--- src/test/java/org/mvnsearch/protobuf/ProtobufNativeJavaTest.java
+++ src/test/java/org/mvnsearch/protobuf/ProtobufNativeJavaTest.java
@@ -1,9 +1,14 @@
 package org.mvnsearch.protobuf;
 
 import com.google.protobuf.Any;
+import com.google.protobuf.DescriptorProtos;
+import com.google.protobuf.Descriptors;
+import com.google.protobuf.TextFormat;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mvnsearch.user.UserPB;
+
+import java.util.Map;
 
 /**
  * Protobuf native java test
@@ -19,6 +24,7 @@ public class ProtobufNativeJavaTest {
                 .setName("Jackie")
                 .setEmail("demo@demo.com")
                 .setPhone("186").build();
+        System.out.println(TextFormat.printer().printToString(user));
         byte[] pbBytes = user.toByteArray();
         UserPB pbUser = UserPB.parseFrom(pbBytes);
         System.out.println(pbUser.getName());
@@ -36,5 +42,19 @@ public class ProtobufNativeJavaTest {
         Any any = Any.parseFrom(any1.toByteArray());
         UserPB user2 = any.unpack(UserPB.class);
         System.out.println(user2.getName());
+    }
+
+    @Test
+    public void testMetadata() {
+        final Descriptors.Descriptor descriptor = UserPB.getDescriptor();
+        final DescriptorProtos.MessageOptions options = descriptor.getOptions();
+        final Map<Descriptors.FieldDescriptor, Object> allFields = options.getAllFields();
+        for (Descriptors.FieldDescriptor fieldDescriptor : allFields.keySet()) {
+            final Object field = descriptor.getOptions().getField(fieldDescriptor);
+            System.out.println(field);
+        }
+        for (Descriptors.FieldDescriptor field : descriptor.getFields()) {
+            System.out.println(field.getName());
+        }
     }
 }
