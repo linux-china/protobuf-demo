cac4d12077f4983dc707dc449b4b051cfb4f65c6
linux_china <libing.chen@gmail.com>
2021-10-07 13:47:09 -0700

    Introduce ProtobufUtil

--- src/test/java/org/mvnsearch/protobuf/ProtoStreamTest.java
+++ src/test/java/org/mvnsearch/protobuf/ProtoStreamTest.java
@@ -1,37 +1,19 @@
 package org.mvnsearch.protobuf;
 
 
-import org.infinispan.protostream.ProtobufTagMarshaller;
+import org.infinispan.protostream.ProtobufUtil;
 import org.infinispan.protostream.SerializationContext;
-import org.infinispan.protostream.config.Configuration;
-import org.infinispan.protostream.impl.SerializationContextImpl;
-import org.infinispan.protostream.impl.TagReaderImpl;
-import org.infinispan.protostream.impl.TagWriterImpl;
 import org.junit.jupiter.api.Test;
-
-import java.io.ByteArrayOutputStream;
 
 public class ProtoStreamTest {
 
     @Test
     public void testProtoStream() throws Exception {
-        AccountDO accountDO = new AccountDO(1, "demo");
-        final SerializationContext serCtx = serializationContext();
-        final ProtobufTagMarshaller<AccountDO> marshaller = (ProtobufTagMarshaller<AccountDO>) serCtx.getMarshaller(AccountDO.class);
-        final ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        final TagWriterImpl writer = TagWriterImpl.newInstance(serCtx, bos);
-        marshaller.write(writer, accountDO);
-        writer.flush();
-        System.out.println(bos.toByteArray().length);
-        final AccountDO accountDO2 = marshaller.read(TagReaderImpl.newInstance(serCtx, bos.toByteArray()));
+        final SerializationContext serCtx = ProtobufUtil.newSerializationContext();
+        new LibraryInitializerImpl().registerMarshallers(serCtx);
+        AccountDO accountDO = new AccountDO(1, "leijuan", "linux_china@hotmail.com", "186");
+        final byte[] bytes = ProtobufUtil.toByteArray(serCtx, accountDO);
+        AccountDO accountDO2 = ProtobufUtil.fromByteArray(serCtx, bytes, AccountDO.class);
         System.out.println(accountDO2.getName());
-    }
-
-    public SerializationContext serializationContext() {
-        final Configuration configuration = Configuration.builder().build();
-        SerializationContext serCtx = new SerializationContextImpl(configuration);
-        LibraryInitializer libraryInitializer = new LibraryInitializerImpl();
-        libraryInitializer.registerMarshallers(serCtx);
-        return serCtx;
     }
 }
